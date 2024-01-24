abfddfdbf22abb51e578a7470429e8da4329ae90
Boris KIEFFER <boriskieffer@outlook.fr>
2024-01-24 21:48:32 +0100

    initial pelican configuaration

---
+++ content/test.md
@@ -0,0 +1,5 @@
+Title: Hello world
+Date: 01/01/1337
+slug: hello-world
+
+Hello world
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+AUTHOR = 'Boris K.'
+SITENAME = 'Boris K.'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+"""
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+"""
+
+# Social widget
+SOCIAL = (
+    ("LinkedIn", "https://www.linkedin.com/boriskieffer"),
+    ("GitHub", "https://www.github.com/boriskieffer"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
